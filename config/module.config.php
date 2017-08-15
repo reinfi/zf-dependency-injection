48e32a4aa9394dce4b1280ef3d5ffe2c8598306d
Martin Reinfandt <reinfandt@z-em.de>
2017-08-15 06:05:51 +0200

    add delegator factory to replace service manager within application

--- config/module.config.php
+++ config/module.config.php
@@ -3,13 +3,13 @@
 return [
     'service_manager' => [
         'factories'          => [
-            \Reinfi\DependencyInjection\Config\ModuleConfig::class                          => \Reinfi\DependencyInjection\Config\Factory\ModuleConfigFactory::class,
-            \Reinfi\DependencyInjection\Service\Extractor\AnnotationExtractor::class        => \Reinfi\DependencyInjection\Service\Extractor\Factory\AnnotationExtractorFactory::class,
-            \Reinfi\DependencyInjection\Service\Extractor\YamlExtractor::class              => \Reinfi\DependencyInjection\Service\Extractor\Factory\YamlExtractorFactory::class,
-            \Reinfi\DependencyInjection\Service\InjectionService::class                     => \Reinfi\DependencyInjection\Service\Factory\InjectionServiceFactory::class,
-            \Reinfi\DependencyInjection\Service\CacheService::class                         => \Reinfi\DependencyInjection\Service\Factory\CacheServiceFactory::class,
-            \Reinfi\DependencyInjection\Service\ConfigService::class                        => \Reinfi\DependencyInjection\Service\Factory\ConfigServiceFactory::class,
-            \Reinfi\DependencyInjection\Service\AutoWiringService::class                     => \Reinfi\DependencyInjection\Service\Factory\AutoWiringServiceFactory::class,
+            \Reinfi\DependencyInjection\Config\ModuleConfig::class                               => \Reinfi\DependencyInjection\Config\Factory\ModuleConfigFactory::class,
+            \Reinfi\DependencyInjection\Service\Extractor\AnnotationExtractor::class             => \Reinfi\DependencyInjection\Service\Extractor\Factory\AnnotationExtractorFactory::class,
+            \Reinfi\DependencyInjection\Service\Extractor\YamlExtractor::class                   => \Reinfi\DependencyInjection\Service\Extractor\Factory\YamlExtractorFactory::class,
+            \Reinfi\DependencyInjection\Service\InjectionService::class                          => \Reinfi\DependencyInjection\Service\Factory\InjectionServiceFactory::class,
+            \Reinfi\DependencyInjection\Service\CacheService::class                              => \Reinfi\DependencyInjection\Service\Factory\CacheServiceFactory::class,
+            \Reinfi\DependencyInjection\Service\ConfigService::class                             => \Reinfi\DependencyInjection\Service\Factory\ConfigServiceFactory::class,
+            \Reinfi\DependencyInjection\Service\AutoWiringService::class                         => \Reinfi\DependencyInjection\Service\Factory\AutoWiringServiceFactory::class,
             \Reinfi\DependencyInjection\Service\Extractor\ExtractorInterface::class              => \Reinfi\DependencyInjection\Service\Extractor\Factory\ExtractorFactory::class,
             \Reinfi\DependencyInjection\Service\AutoWiring\ResolverService::class                => \Reinfi\DependencyInjection\Service\AutoWiring\Factory\ResolverServiceFactory::class,
             \Reinfi\DependencyInjection\Service\AutoWiring\Resolver\ContainerResolver::class     => \Reinfi\DependencyInjection\Service\AutoWiring\Resolver\Factory\ContainerResolverFactory::class,
@@ -17,6 +17,11 @@ return [
         ],
         'abstract_factories' => [
             \Reinfi\DependencyInjection\AbstractFactory\Config\InjectConfigAbstractFactory::class,
+        ],
+        'delegators'         => [
+            'Application' => [
+                \Reinfi\DependencyInjection\Factory\Application\ApplicationFactory::class,
+            ],
         ],
     ],
     'console'         => [
