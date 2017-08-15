f3ef18be8490c5870612a6435d4b389d0437c800
Martin Reinfandt <reinfandt@z-em.de>
2017-08-15 06:05:20 +0200

    replace service manager after modules are loaded

--- src/Module.php
+++ src/Module.php
@@ -2,12 +2,16 @@
 
 namespace Reinfi\DependencyInjection;
 
+use Reinfi\DependencyInjection\Service\Optimizer\OptimizerService;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\InitProviderInterface;
+use Zend\ModuleManager\ModuleEvent;
+use Zend\ModuleManager\ModuleManagerInterface;
 
 /**
  * @package Reinfi\DependencyInjection
  */
-class Module implements ConfigProviderInterface
+class Module implements ConfigProviderInterface, InitProviderInterface
 {
     /**
      * @inheritDoc
@@ -17,4 +21,30 @@ class Module implements ConfigProviderInterface
         return require __DIR__ . '/../config/module.config.php';
     }
 
+    /**
+     * @param ModuleManagerInterface $manager
+     */
+    public function init(ModuleManagerInterface $manager)
+    {
+        $manager->getEventManager()->attach(
+            ModuleEvent::EVENT_LOAD_MODULES_POST,
+            [
+                $this,
+                'replaceServiceManager',
+            ],
+            PHP_INT_MAX
+        );
+    }
+
+    /**
+     * @param ModuleEvent $event
+     */
+    public function replaceServiceManager(ModuleEvent $event)
+    {
+        $container = $event->getParam('ServiceManager');
+
+        if (class_exists(OptimizerService::SERVICE_MANAGER_FQCN)) {
+            $event->setParam('ServiceManager', new OptimizedServiceManager($container));
+        }
+    }
 }
