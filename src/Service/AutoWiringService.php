6102dabe662f5c2c8898b00094194420d410629d
Martin Reinfandt <reinfandt@z-em.de>
2017-08-20 19:07:35 +0200

    remove not needed else from autowiring service

--- src/Service/AutoWiringService.php
+++ src/Service/AutoWiringService.php
@@ -3,9 +3,6 @@
 namespace Reinfi\DependencyInjection\Service;
 
 use Psr\Container\ContainerInterface;
-use ReflectionClass;
-use ReflectionParameter;
-use Reinfi\DependencyInjection\Injection\AutoWiring;
 use Reinfi\DependencyInjection\Injection\InjectionInterface;
 use Reinfi\DependencyInjection\Service\AutoWiring\ResolverService;
 use Reinfi\DependencyInjection\Traits\CacheKeyTrait;
@@ -50,15 +47,7 @@ class AutoWiringService
         ContainerInterface $container,
         string $className
     ) {
-        $cacheKey = $this->buildCacheKey($className);
-
-        /** @var InjectionInterface[] $injections */
-        if ($this->cache->hasItem($cacheKey)) {
-            $injections = $this->cache->getItem($cacheKey);
-        } else {
-            $injections = $this->resolverService->resolve($className);
-            $this->cache->setItem($cacheKey, $injections);
-        }
+        $injections = $this->getInjections($className);
 
         if (count($injections) === 0) {
             return false;
@@ -67,6 +56,25 @@ class AutoWiringService
         foreach ($injections as $index => $injection) {
             $injections[$index] = $injection($container);
         }
+
+        return $injections;
+    }
+
+    /**
+     * @param string $className
+     *
+     * @return InjectionInterface[]
+     */
+    protected function getInjections(string $className): array
+    {
+        $cacheKey = $this->buildCacheKey($className);
+
+        if ($this->cache->hasItem($cacheKey)) {
+            return $this->cache->getItem($cacheKey);
+        }
+
+        $injections = $this->resolverService->resolve($className);
+        $this->cache->setItem($cacheKey, $injections);
 
         return $injections;
     }
