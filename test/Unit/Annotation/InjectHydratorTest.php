3fd6c8bef28dd487ae769be580c685b23459b7b0
Martin Reinfandt <reinfandt@z-em.de>
2017-11-18 19:58:45 +0100

    adjust tests to work with new annotation values

--- test/Unit/Annotation/InjectHydratorTest.php
+++ test/Unit/Annotation/InjectHydratorTest.php
@@ -5,7 +5,7 @@ namespace Reinfi\DependencyInjection\Unit\Annotation;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
 use Reinfi\DependencyInjection\Annotation\InjectHydrator;
-use Reinfi\DependencyInjection\Service\InjectionService;
+use Reinfi\DependencyInjection\Service\Service1;
 use Zend\ServiceManager\AbstractPluginManager;
 
 /**
@@ -15,16 +15,27 @@ class InjectHydratorTest extends TestCase
 {
     /**
      * @test
+     *
+     * @dataProvider getAnnotationValues
+     *
+     * @param array  $values
+     * @param string $className
      */
-    public function itCallsPluginManagerWithValue()
-    {
-        $inject = new InjectHydrator();
-
-        $inject->value = InjectionService::class;
+    public function itCallsPluginManagerWithValue(
+        array $values,
+        string $className
+    ) {
+        $inject = new InjectHydrator($values);
 
         $pluginManager = $this->prophesize(AbstractPluginManager::class);
-        $pluginManager->get(InjectionService::class)
-            ->willReturn(true);
+
+        if (isset($values['options'])) {
+            $pluginManager->get($className, $values['options'])
+                ->willReturn(true);
+        } else {
+            $pluginManager->get($className)
+                ->willReturn(true);
+        }
 
         $container = $this->prophesize(ContainerInterface::class);
         $container->get('HydratorManager')
@@ -38,18 +49,30 @@ class InjectHydratorTest extends TestCase
 
     /**
      * @test
+     *
+     * @dataProvider getAnnotationValues
+     *
+     * @param array  $values
+     * @param string $className
      */
-    public function itCallsPluginManagerFromParentServiceLocator()
-    {
-        $inject = new InjectHydrator();
-
-        $inject->value = InjectionService::class;
+    public function itCallsPluginManagerFromParentServiceLocator(
+        array $values,
+        string $className
+    ) {
+        $inject = new InjectHydrator($values);
 
         $filterManager = $this->prophesize(AbstractPluginManager::class);
-        $filterManager->get(InjectionService::class)
-            ->willReturn(true);
+
+        if (isset($values['options'])) {
+            $filterManager->get($className, $values['options'])
+                ->willReturn(true);
+        } else {
+            $filterManager->get($className)
+                ->willReturn(true);
+        }
 
         $container = $this->prophesize(ContainerInterface::class);
+
         $container->get('HydratorManager')
             ->willReturn($filterManager->reveal());
 
@@ -61,5 +84,33 @@ class InjectHydratorTest extends TestCase
             $inject($pluginManager->reveal()),
             'Invoke should return true'
         );
+    }
+
+    /**
+     * @return array
+     */
+    public function getAnnotationValues(): array
+    {
+        return [
+            [
+                [
+                    'value' => Service1::class,
+                ],
+                Service1::class,
+            ],
+            [
+                [
+                    'name'    => Service1::class,
+                    'options' => [ 'field' => true ],
+                ],
+                Service1::class,
+            ],
+            [
+                [
+                    'name' => Service1::class,
+                ],
+                Service1::class,
+            ],
+        ];
     }
 }
