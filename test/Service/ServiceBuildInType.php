c8ff119e9f29b112012e6c4c123f80b1d1f6a9ce
Reinfi <reinfandt@z-em.de>
2017-11-28 06:33:12 +0100

    add more test service classes

---
+++ test/Service/ServiceBuildInType.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Reinfi\DependencyInjection\Service;
+
+/**
+ * @package Reinfi\DependencyInjection\Service
+ */
+class ServiceBuildInType
+{
+    /**
+     * @param int $service2
+     */
+    public function __construct(
+        int $service2
+    ) {
+    }
+}
