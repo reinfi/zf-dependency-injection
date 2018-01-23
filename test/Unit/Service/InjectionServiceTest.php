f838d69ed9ad3bb5ed790be9b43051e82a51d46e
Reinfi <reinfandt@z-em.de>
2018-01-23 23:26:46 +0100

    add tests for cached item check

--- test/Unit/Service/InjectionServiceTest.php
+++ test/Unit/Service/InjectionServiceTest.php
@@ -140,6 +140,47 @@ class InjectionServiceTest extends TestCase
     /**
      * @test
      */
+    public function itUsesExtractorWhenCacheItemIsNotAnArray()
+    {
+        $cacheKey = $this->buildCacheKey(Service1::class);
+        $extractor = $this->prophesize(ExtractorInterface::class);
+
+        $injection = $this->prophesize(InjectionInterface::class);
+        $injection->addMethodProphecy(
+            (new MethodProphecy($injection, '__invoke', [Argument::type(ContainerInterface::class)]))
+            ->willReturn(new Service2())
+        );
+
+        $extractor->getConstructorInjections(Service1::class)
+            ->willReturn([]);
+        $extractor->getPropertiesInjections(Service1::class)
+            ->willReturn([
+                             $injection->reveal(),
+                         ]);
+
+        $cache = $this->prophesize(CacheService::class);
+        $cache->hasItem($cacheKey)->willReturn(true)->shouldBeCalled();
+        $cache->getItem($cacheKey)->willReturn(null)->shouldBeCalled();
+        $cache->setItem($cacheKey, Argument::type('array'))->willReturn(true);
+
+        $service = new InjectionService(
+            $extractor->reveal(),
+            $cache->reveal()
+        );
+
+        $container = $this->prophesize(ContainerInterface::class);
+
+        $injections = $service->resolveConstructorInjection(
+            $container->reveal(),
+            Service1::class
+        );
+
+        $this->assertCount(1, $injections);
+    }
+
+    /**
+     * @test
+     */
     public function itReturnsFalseWhenNoInjectionsAvaible()
     {
         $cacheKey = $this->buildCacheKey(Service2::class);
