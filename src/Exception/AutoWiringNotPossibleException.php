548458a35fb4719d5193730127846b909df45ad9
Christian Blank <christian.blank@check24.de>
2018-08-01 17:15:11 +0200

    [#17] Add better error messages in resolver

--- src/Exception/AutoWiringNotPossibleException.php
+++ src/Exception/AutoWiringNotPossibleException.php
@@ -12,14 +12,17 @@ class AutoWiringNotPossibleException extends \Exception
     /**
      * @param \ReflectionClass $reflClass
      *
+     * @param \ReflectionClass $constructedClass
+     *
      * @return AutoWiringNotPossibleException
      */
-    public static function fromClassName(\ReflectionClass $reflClass): self
+    public static function fromClassName(\ReflectionClass $reflClass, \ReflectionClass $constructedClass): self
     {
         return new self(
             sprintf(
-                'Could not resolve class %s',
-                $reflClass->getName()
+                'Could not resolve class %s to inject into class %s',
+                $reflClass->getName(),
+                $constructedClass->getName()
             )
         );
     }
@@ -27,14 +30,19 @@ class AutoWiringNotPossibleException extends \Exception
     /**
      * @param \ReflectionParameter $reflParameter
      *
+     * @param \ReflectionClass     $constructedClass
+     *
      * @return AutoWiringNotPossibleException
      */
-    public static function fromMissingTypeHint(\ReflectionParameter $reflParameter): self
-    {
+    public static function fromMissingTypeHint(
+        \ReflectionParameter $reflParameter,
+        \ReflectionClass $constructedClass
+    ): self {
         return new self(
             sprintf(
-                "Could not resolve variable %s as it is missing a typehint",
-                $reflParameter->getName()
+                'Could not resolve variable %s as it is missing a typehint to inject into class %s',
+                $reflParameter->getName(),
+                $constructedClass->getName()
             )
         );
     }
@@ -42,14 +50,19 @@ class AutoWiringNotPossibleException extends \Exception
     /**
      * @param \ReflectionParameter $reflParameter
      *
+     * @param \ReflectionClass     $constructedClass
+     *
      * @return AutoWiringNotPossibleException
      */
-    public static function fromBuildInType(\ReflectionParameter $reflParameter): self
-    {
+    public static function fromBuildInType(
+        \ReflectionParameter $reflParameter,
+        \ReflectionClass $constructedClass
+    ): self {
         return new self(
             sprintf(
-                "Could not resolve variable %s as it is of a buildin type",
-                $reflParameter->getName()
+                'Could not resolve variable %s as it is of a buildin type to inject into class %s',
+                $reflParameter->getName(),
+                $constructedClass->getName()
             )
         );
     }
