23dcb2168c583a6ee330e9881c839fe145999e31
Reinfi <reinfandt@z-em.de>
2022-02-19 16:57:46 +0100

    extract container functino to trait

--- src/Attribute/AbstractAttribute.php
+++ src/Attribute/AbstractAttribute.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace Reinfi\DependencyInjection\Attribute;
 
-use Psr\Container\ContainerInterface;
-use Laminas\ServiceManager\AbstractPluginManager;
+use Reinfi\DependencyInjection\Injection\ContainerTrait;
 use Reinfi\DependencyInjection\Injection\InjectionInterface;
 
 /**
@@ -13,13 +12,5 @@ use Reinfi\DependencyInjection\Injection\InjectionInterface;
  */
 abstract class AbstractAttribute implements InjectionInterface
 {
-    protected function determineContainer(ContainerInterface $container): ContainerInterface
-    {
-        if ($container instanceof AbstractPluginManager) {
-            return $container
-                ->getServiceLocator();
-        }
-
-        return $container;
-    }
+    use ContainerTrait;
 }
