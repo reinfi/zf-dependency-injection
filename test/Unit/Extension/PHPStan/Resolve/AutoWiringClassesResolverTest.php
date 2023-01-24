cbfc162fdaac11e5bc08fcdf02d545e7f03db277
Reinfi <reinfandt@z-em.de>
2023-01-24 16:51:34 +0100

    add github actions

---
+++ test/Unit/Extension/PHPStan/Resolve/AutoWiringClassesResolverTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Reinfi\DependencyInjection\Test\Unit\Extension\PHPStan\Resolve;
+
+use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Reinfi\DependencyInjection\Extension\PHPStan\Resolve\AutoWiringClassesResolver;
+use Reinfi\DependencyInjection\Extension\PHPStan\ServiceManagerLoader;
+use Reinfi\DependencyInjection\Test\Service\Service1;
+use Reinfi\DependencyInjection\Test\Service\Service2;
+
+class AutoWiringClassesResolverTest extends TestCase
+{
+    use ProphecyTrait;
+
+    /**
+     * @test
+     */
+    public function itReturnsFalseIfNoServiceManagerIsProvided(): void
+    {
+        $serviceManagerLoader = new ServiceManagerLoader(null);
+
+        $classesResolver = new AutoWiringClassesResolver($serviceManagerLoader);
+
+        self::assertFalse(
+            $classesResolver->isAutowired(Service1::class)
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function itReturnsTrueIfClassIsRegisteredForAutoWiring(): void
+    {
+        $serviceManagerLoader = new ServiceManagerLoader(
+            __DIR__ . '/../../../../resources/container.php'
+        );
+
+        $classesResolver = new AutoWiringClassesResolver($serviceManagerLoader);
+
+        self::assertTrue(
+            $classesResolver->isAutowired(Service1::class)
+        );
+
+        self::assertFalse(
+            $classesResolver->isAutowired(Service2::class)
+        );
+    }
+}
