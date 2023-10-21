717c52bfa719d402bb570b7d5fa8542ef12f4ec4
Reinfi <reinfandt@z-em.de>
2023-10-21 14:59:00 +0200

    fix duplicate test

--- test/Unit/Service/Extractor/Factory/ExtractorFactoryTest.php
+++ test/Unit/Service/Extractor/Factory/ExtractorFactoryTest.php
@@ -112,57 +112,6 @@ class ExtractorFactoryTest extends TestCase
         self::assertContainsOnlyInstancesOf(ExtractorInterface::class, $chain);
     }
 
-    public function testItReturnsArrayOfExtractorsDefinedInConfig(): void
-    {
-        $isPhp8OrAbove = version_compare(PHP_VERSION, '8.0.0') >= 0;
-
-        $moduleConfig = new Config([
-            'extractor' => [
-                YamlExtractor::class,
-            ],
-        ]);
-
-        $yamlExtractor = $this->prophesize(YamlExtractor::class);
-        $annotationExtractor = $this->prophesize(AnnotationExtractor::class);
-        $attributeExtractor = $this->prophesize(AttributeExtractor::class);
-        $container = $this->prophesize(ContainerInterface::class);
-
-        $container->get(ModuleConfig::class)
-            ->willReturn($moduleConfig->toArray())
-            ->shouldBeCalled();
-        $container->get(YamlExtractor::class)
-            ->willReturn($yamlExtractor->reveal())
-            ->shouldBeCalled();
-        $container->get(AnnotationExtractor::class)
-            ->willReturn($annotationExtractor->reveal())
-            ->shouldBeCalled();
-        $container->get(AttributeExtractor::class)
-            ->willReturn($attributeExtractor->reveal())
-            ->shouldBeCalledTimes($isPhp8OrAbove ? 1 : 0);
-
-        $factory = new ExtractorFactory();
-
-        $extractor = $factory($container->reveal());
-
-        self::assertInstanceOf(ExtractorChain::class, $extractor);
-
-        $reflectionClass = new ReflectionClass($extractor);
-        $chainProperty = $reflectionClass->getProperty('chain');
-        $chainProperty->setAccessible(true);
-
-        $chain = $chainProperty->getValue($extractor);
-
-        self::assertTrue(is_array($chain));
-
-        if ($isPhp8OrAbove) {
-            self::assertCount(3, $chain);
-        } else {
-            self::assertCount(2, $chain);
-        }
-
-        self::assertContainsOnlyInstancesOf(ExtractorInterface::class, $chain);
-    }
-
     public function testItReturnsAnnotationExtractorIfNoneDefined(): void
     {
         $moduleConfig = new Config([]);
